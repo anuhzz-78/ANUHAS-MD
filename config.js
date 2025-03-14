952dcda67cadad0f7be1b27e3b4e24cfbd8f1516
anuhzz-78 <anuhaswihanga983@gmail.com>
2025-03-14 09:52:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~ArchzIJJ#B25hUN3g81A21aVLXetFyeXo2fuB5Qt9rOA4rOdSVgU",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/R4sgKkJG/IMG-20250312-WA0002.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃bot name 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭⚡*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰🎠*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : Bot Name",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
